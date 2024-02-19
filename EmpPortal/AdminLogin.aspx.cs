a90b8835405bd7d353c64fde6ac5852d6d7435ba
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-02-19 15:34:27 +0530

    project day 2

---
+++ EmpPortal/AdminLogin.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EmpPortal
+{
+    public partial class AdminLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text=="admin@gmail.com" && txtPassword.Text=="1234")
+            {
+                Session["Admin"] = "True";
+                Response.Redirect("EmployeeList.aspx");
+            }
+        }
+    }
+}
